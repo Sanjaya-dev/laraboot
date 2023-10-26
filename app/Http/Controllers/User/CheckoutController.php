521853a2e518604fd06cc3d67528cbd38137f239
sanjaya-dev <sanjaya.dev20@gmail.com>
2023-10-26 22:44:52 +0700

    validation

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -6,6 +6,7 @@ use App\Models\Camp;
 use App\Models\Checkout;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\Checkout\Store;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -26,8 +27,14 @@ class CheckoutController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create(Camp $camp)
+    public function create(Camp $camp, Request $request)
     {
+        
+
+        if($camp->isRegistered){
+            $request->session()->flash('error',"You alredy registered on {$camp->title} camp.");
+            return redirect(route('dashboard'));
+        }
         // return $camp;
         return view('checkout.create',[
             'camp' => $camp
@@ -40,9 +47,9 @@ class CheckoutController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Camp $camp)
+    public function store(Store $request, Camp $camp)
     {
-        // $request->all();
+        
         //maping rewuest data
         $data = $request->all();
         $data['user_id'] = Auth::id();
