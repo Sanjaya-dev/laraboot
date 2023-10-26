efc8f897c3e03dfb847a20f4e143c29e762beeab
sanjaya-dev <sanjaya.dev20@gmail.com>
2023-10-26 15:15:07 +0700

    integrasi landing page and my dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -20,20 +21,25 @@ Route::get('/', function () {
 })->name('welcome');
 
 
-Route::get('checkout/success',[CheckoutController::class,'success'])->name('checkout.success');
-Route::get('checkout/{camp:slug}',[CheckoutController::class,'create'])->name('checkout.create');
-Route::post('checkout/{camp}',[CheckoutController::class,'store'])->name('checkout.store');
-
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success_checkout');
+// Route::get('success-checkout', function () {
+//     return view('success_checkout');
+// })->name('success_checkout');
 
 Route::get('sign-in-google', [UserController::class,'google'] )->name('user.login.google');
-
 Route::get('auth/google/callback',[UserController::class,'handleProviderCallback'])->name('user.google.callback');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+// login terlebih dahulu untuk dapat mengakses route tersebut
+Route::middleware(['auth'])->group(function () {
+    // chekcout
+    Route::get('checkout/success',[CheckoutController::class,'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}',[CheckoutController::class,'create'])->name('checkout.create');
+    Route::post('checkout/{camp}',[CheckoutController::class,'store'])->name('checkout.store');
+    // user dashboard
+    Route::get('dashboard',[HomeController::class,'dashboard'])->name('dashboard');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
